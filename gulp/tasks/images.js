e56c8e1d25c711ce69d939d53a6add24bd9d8ae5
Jordan <molongkan@mail.ru>
2023-01-05 02:19:27 +0300

    Commit

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -13,7 +13,6 @@ const images = () => {
             )
         )
         .pipe(app.plugins.newer(app.path.build.images))
-        .pipe(app.plugins.if(app.isBuild, webp()))
         .pipe(app.plugins.if(app.isBuild, app.gulp.dest(app.path.build.images)))
         .pipe(app.plugins.if(app.isBuild, app.gulp.src(app.path.src.images)))
         .pipe(
